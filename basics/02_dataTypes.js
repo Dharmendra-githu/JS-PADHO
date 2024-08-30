e40fd535602700a8a6970dbedf7e330cac4d5f5c
Dharmendra-githu <139001764+Dharmendra-githu@users.noreply.github.com>
2024-08-30 07:26:25 +0000

    DataTypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js  code as newer version 
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); 
+
+
+
+console.log("ankit")
+
+
+let name  = "Ankit"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power of 53
+//bigInt
+// string  => ""
+// boolean => true ya false
+// null => standalone value
+// undefined =>
+    // symbol => uniqness find karne ke liye use karte hai 
+
+
+//object
+
+console.log(typeof undefined);
+
